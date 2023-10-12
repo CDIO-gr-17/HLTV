3488919322fadb5a4807a01c7619e6f30dd10ad6
Jakob-SA <agergaardskovjakob@gmail.com>
2023-10-12 11:51:44 +0200

    Merge remote-tracking branch 'origin/Dev' into Dev
    # Conflicts:
    #	app/src/main/java/com/example/hltv/screens/Events.kt
    #	app/src/main/java/com/example/hltv/screens/Home.kt
    #	app/src/main/java/com/example/hltv/screens/Matches.kt
    #	app/src/main/java/com/example/hltv/screens/News.kt
    #	app/src/main/java/com/example/hltv/screens/Ranking.kt

--- app/src/main/java/com/example/hltv/MainActivity.kt
+++ app/src/main/java/com/example/hltv/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = colorScheme.background
                 ) {
                     Greeting("Android")
                 }
