1264013514f80cdfade175720ba8398e6ebbf312
Anton <113907270+s224268@users.noreply.github.com>
2023-10-12 15:15:21 +0200

    There is now a list of some sort thing

--- app/src/main/java/com/example/hltv/screens/Ranking.kt
+++ app/src/main/java/com/example/hltv/screens/Ranking.kt
@@ -1,49 +1,69 @@
 package com.example.hltv.screens
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.hltv.Greeting
-import com.example.hltv.ui.theme.HLTVTheme
-
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import com.example.hltv.R
 
 val items = (1..20).map { index ->
     ListItem(index, "Item $index Text 1", "Item $index Text 2")
 }
-
 data class ListItem(val ranking: Int, val text1: String, val text2: String)
 
 @Composable
 fun ItemList(items: List<ListItem>) {
     LazyColumn {
-        items(items.size) { index ->
-            val item = items[index]
-            ItemRow(item = item)
+        items(items.size) {index ->
+            CardRow(team = "Astralis", subtext = "RUSH B", index+1)
+            if (index < items.size-1){
+                Spacer(modifier = Modifier.height(1.dp))
+            }
         }
     }
 }
 
 @Composable
-fun ItemRow(item: ListItem) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "${item.ranking}.",
-            modifier = Modifier.padding(16.dp),
-            //style = MaterialTheme.typography.h6
-        )
-        Column {
-            Text(text = item.text1, style = MaterialTheme.typography.bodyLarge)
-            Text(text = item.text2, style = MaterialTheme.typography.bodyMedium)
+fun CardRow(team: String, subtext: String, index: Int) {Card(
+    colors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+    ),
+    modifier = Modifier
+        .fillMaxWidth()
+        .height(80.dp)
+        ){
+            Row(modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically) {
+                Text("#$index ",
+                    fontSize = 36.sp
+                    )
+                //Image(bitmap = artist.image, contentDescription = "Artist image")
+                Column (){
+                    Text(team, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 4.dp), fontSize = 16.sp)
+                    Text(subtext, fontSize = 14.sp)
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.astralis_logo),
+                    contentDescription = null, //TODO
+                    alignment = Alignment.TopEnd,
+                    modifier = Modifier.fillMaxSize().padding(2.dp)//.padding(start = 120.dp)
+                )
+            }
         }
-    }
 }
 
 @Preview(showBackground = true)
