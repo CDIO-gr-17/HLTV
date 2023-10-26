a9a3a7152ff0dfe0a55a47b6522f35c2bfbee1f9
Jakob-SA <agergaardskovjakob@gmail.com>
2023-10-26 17:49:08 +0200

    Simple settingsToggle function created

--- app/src/main/java/com/example/hltv/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/hltv/screens/SettingsScreen.kt
@@ -1,8 +1,47 @@
 package com.example.hltv.screens
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SettingsScreen() {
+    var darkModeEnabled by remember {
+        mutableStateOf(false)
+    }
+    Surface (modifier = Modifier.padding(16.dp)) {
+        SettingsToggle(settingName = "Dark mode",
+            isChecked = darkModeEnabled,
+            onCheckedChange = { darkModeEnabled = it })
 
+    }
+
+}
+
+@Composable
+fun SettingsToggle(
+    settingName: String,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+    ) {
+    Row (modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically)
+    {
+        Text(text = settingName)
+
+        Switch(checked = isChecked, onCheckedChange =onCheckedChange)
+    }
 }
