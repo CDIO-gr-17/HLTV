3b3486b1812a917a2564d800f0dcd26953c0fb9a
Philip <philipmuff@gmail.com>
2023-11-07 14:05:13 +0100

    Added image to the commonCard

--- app/src/main/java/com/example/hltv/ui/common/CommonComposable.kt
+++ app/src/main/java/com/example/hltv/ui/common/CommonComposable.kt
@@ -1,13 +1,19 @@
 package com.example.hltv.ui.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -119,27 +126,43 @@ fun commonCard(
                 .fillMaxWidth()
                 .height(IntrinsicSize.Max)
                 .padding(8.dp)
-        ) {if(topBox != null){
-            Box() {
-                topBox()
-            }
-        }else
-            Column {
-                headText?.let {
-                    Text(
-                        text = it,
-                        fontSize = R.typography.bodyLarge.fontSize,
-                        color = R.colorScheme.onSecondary,
+        ) {
+            if (topBox != null) {
+                Box() {
+                    topBox()
+                }
+            } else
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+
+                    Column (
+                        modifier = Modifier.weight(1f)
+                    ){
+                        headText?.let {
+                            Text(
+                                text = it,
+                                fontSize = R.typography.bodyLarge.fontSize,
+                                color = R.colorScheme.onSecondary,
+                            )
+                        }
+                        subText?.let {
+                            Text(
+                                text = it,
+                                fontSize = R.typography.bodyMedium.fontSize,
+                                color = R.colorScheme.onSecondary,
+                            )
+                        }
+                    }
+                    Image(
+                        painter = painterResource(id = com.example.hltv.R.drawable.astralis_logo),
+                        contentDescription = null,
+                        alignment = Alignment.CenterEnd,
+                        modifier = Modifier
+                            .size(40.dp) //Burde ikke være hardcoded, da det ikke ville passe uden subText
                     )
                 }
-                subText?.let {
-                    Text(
-                        text = it,
-                        fontSize = R.typography.bodyMedium.fontSize,
-                        color = R.colorScheme.onSecondary,
-                    )
-                }
-            }
         }
         bottomBox?.let { bottomBox ->
             Box(
