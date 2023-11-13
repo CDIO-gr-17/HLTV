724c56e6b870138ced112d1b2039b3716a7b0488
Anton <s224268@student.dtu.dk>
2023-11-13 12:24:30 +0100

    Prettified the functions

--- app/src/main/java/com/example/hltv/data/remote/API.kt
+++ app/src/main/java/com/example/hltv/data/remote/API.kt
@@ -2,22 +2,7 @@ package com.example.hltv.data.remote
 import okhttp3.Request
 
 const val APIKEY = "0bb790a9cemsh84cfcecde257781p164a86jsn329610f90740"
-const val ONLYCS = true //This seems unnecessary but we ball
-
-
-/*
-sealed class APIResponse{
-    data class EventsWrapper(val event: List<Event>) : APIResponse()
-    data class Lineup(val event: List<Event>) : APIResponse()
-
-    //More to be added here
-}
-
- */
-/**
- * @return: Returns an object of type eventsWrapper, which just contains a list of "event",
- * event being the api's name for a match
- */
+const val ONLYCS = false //This seems unnecessary but we ball
 
 /**
  * Returns live matches
@@ -43,7 +28,7 @@ fun getLiveMatches(): APIResponse.EventsWrapper? {
  */
 fun getPlayersFromEvent(eventID: Int? = 10945127): APIResponse.Lineup {
     print(eventID)
-    return getAPIResponse("event/" + eventID.toString() + "/lineups", APIKEY,APIResponse.Lineup::class.java) as APIResponse.Lineup
+    return getAPIResponse("event/" + eventID.toString() + "/lineups", APIKEY, APIResponse.Lineup::class.java) as APIResponse.Lineup
 }
 
 
@@ -54,9 +39,10 @@ fun getPreviousMatches(teamID: Int, pageID: Int = 0 ):APIResponse.EventsWrapper{
     return getAPIResponse("team/"+teamID.toString()+"/matches/previous/"+ pageID, APIKEY, APIResponse.EventsWrapper::class.java) as APIResponse.EventsWrapper
 }
 
-
-private fun getAPIResponse(apiURL: String, apiKEY: String, classtopass:Class<*>): APIResponse {
-
+/**
+ * @param desiredClass The class to pass to gson. This is the same as your return class, e.g. APIResponse.Lineup::class.java
+ */
+private fun getAPIResponse(apiURL: String, apiKEY: String, desiredClass:Class<*>): APIResponse {
 
     val request = Request.Builder()
         .url("https://allsportsapi2.p.rapidapi.com/api/esport/$apiURL")
@@ -69,19 +55,18 @@ private fun getAPIResponse(apiURL: String, apiKEY: String, classtopass:Class<*>)
     val response = client.newCall(request).execute()
     // Get the HTTP response as a string
     val jsonString = response.body()?.string()
+    response.close()
 
     //Initiating as late as possible for performance reasons. Don't think it makes much of a difference
     val gson = GsonSingleton.instance
-    //val regexForPlayersFromEvent = Regex("event/[0-9]+/lineups")
-
-    response.close()
-
-    return gson.fromJson(jsonString, classtopass) as APIResponse
+    return gson.fromJson(jsonString, desiredClass) as APIResponse
 }
 
 fun main() {
 
     val a = getPreviousMatches(364425,0)
+    val b = getLiveMatches()
+    val c = getPlayersFromEvent(b?.events?.get(0)?.id)
     print(a)
 
 
