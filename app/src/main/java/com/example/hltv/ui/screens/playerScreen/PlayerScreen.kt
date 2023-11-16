2797840db343f9f2f78ea8ff70e53a9f56a038d9
Philip <philipmuff@gmail.com>
2023-11-16 11:32:10 +0100

    Added playerImage to the PlayerScreen

--- app/src/main/java/com/example/hltv/ui/screens/playerScreen/PlayerScreen.kt
+++ app/src/main/java/com/example/hltv/ui/screens/playerScreen/PlayerScreen.kt
@@ -1,2 +1,45 @@
 package com.example.hltv.ui.screens.playerScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.hltv.R
+
+
+@Composable
+fun PlayerScreen(){
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ){
+        PlayerImage(image = painterResource(id = R.drawable.person_24px))
+    }
+}
+
+@Composable
+fun PlayerImage(
+    image: Painter,
+){
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)
+    )
+}
+
+@Preview
+@Composable
+fun PlayerScreenPreview(){
+    PlayerScreen()
+}
