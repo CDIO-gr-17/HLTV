df2d66b9d78c8a432712cc3ca65f468cef88bb0f
Jakob-SA <agergaardskovjakob@gmail.com>
2023-11-28 11:53:22 +0100

    Merged testbranch into dev

--- app/src/main/java/com/example/hltv/ui/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/hltv/ui/screens/homeScreen/HomeScreen.kt
@@ -2,9 +2,7 @@
 
 package com.example.hltv.ui.screens.homeScreen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,8 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Delete
@@ -27,13 +23,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.hltv.R
-import com.example.hltv.ui.common.CommonCard
+import coil.compose.rememberAsyncImagePainter
+import com.example.hltv.ui.common.LiveMatchCard
 
 val M = MaterialTheme
 
--- app/src/main/java/com/example/hltv/ui/screens/matchesScreen/MatchesScreen.kt
+++ app/src/main/java/com/example/hltv/ui/screens/matchesScreen/MatchesScreen.kt
@@ -20,16 +20,18 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import com.example.hltv.R
-import com.example.hltv.ui.screens.homeScreen.LiveMatchCard
+import com.example.hltv.ui.common.LiveMatchCard
 
 
 data class ListItem(val ranking: Int, val text1: String, val text2: String)
