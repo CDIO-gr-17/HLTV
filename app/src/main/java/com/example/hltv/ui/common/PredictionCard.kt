7ea55b49cd26376487aa5dd268cc1c489d91f6f1
Jakob-SA <agergaardskovjakob@gmail.com>
2024-01-02 14:36:25 +0100

    Design done. Functionality needed

--- app/src/main/java/com/example/hltv/ui/common/PredictionCard.kt
+++ app/src/main/java/com/example/hltv/ui/common/PredictionCard.kt
@@ -1,11 +1,15 @@
 package com.example.hltv.ui.common
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,107 +24,122 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PredictionCard(
     modifier: Modifier = Modifier,
     teamOneIcon: Painter,
     teamTwoIcon: Painter,
-    teamOneColor: Color,
-    teamTwoColor: Color,
+    teamOneColor: Color = Color.Blue,
+    teamTwoColor: Color = Color.Red,
 ) {
-    CommonCard(
-        modifier = modifier,
-        customInnerPadding = 0.dp,
-        topBox = {
-            Box(
-                modifier = Modifier
-                    .background(color = MaterialTheme.colorScheme.primaryContainer)
-                    .fillMaxWidth()
-                    .padding(8.dp)
+    CommonCard(modifier = modifier, customInnerPadding = 0.dp, topBox = {
+        Box(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
+                Column(
+                    modifier = Modifier.weight(1f)
                 ) {
-
-                    Column(
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        Text(
-                            text = "Get your prediction in!",
-                            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-                            color = MaterialTheme.colorScheme.onPrimaryContainer,
-                        )
-                        Text(
-                            text = "Vote for the team you think will win",
-                            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-                            color = MaterialTheme.colorScheme.onPrimaryContainer,
-                        )
-                    }
-
+                    Text(
+                        text = "Get your prediction in!",
+                        fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    )
+                    Text(
+                        text = "Vote for the team you think will win",
+                        fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    )
                 }
+
             }
-        },
-        bottomBox = {
-            Box(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .background(
-                        brush = Brush.horizontalGradient(
-                            listOf(
-                                teamOneColor,
-                                teamTwoColor
-                            )
+        }
+    }, bottomBox = {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.horizontalGradient(
+                        listOf(
+                            teamOneColor, teamTwoColor
                         )
                     )
-            ) {
-                Row(modifier = modifier
+                )
+        ) {
+            Row(
+                modifier = modifier
                     .padding(8.dp)
                     .fillMaxWidth()
+                    .align(Alignment.Center),
+                horizontalArrangement = Arrangement.SpaceAround
 
-                ) {
-                    Box(
-                        modifier = modifier
-                            .size(100.dp)
-                            .border(2.dp, Color.Black, CircleShape)
-                            .clip(CircleShape)
-                            .background(MaterialTheme.colorScheme.onPrimaryContainer)
+            ) {
 
+                drawCircle(
+                    modifier = modifier, teamIcon = teamOneIcon,
+                    onClick = {}
+
+                )
+                Text(
+                    text = "VS",
+                    modifier = modifier
+                        .padding(8.dp)
+                        .align(Alignment.CenterVertically),
+                    fontSize = 65.sp,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    textAlign = TextAlign.Center,
+                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
                     )
-                    Box(
-                        modifier = modifier
-                            .clip(CircleShape)
-                            .background(MaterialTheme.colorScheme.onPrimaryContainer)
-                            .size(100.dp)
-                            .padding(8.dp)
-                    )
-
-                }
-
+                drawCircle(
+                    modifier = modifier, teamIcon = teamTwoIcon,onClick = {}
+                )
 
             }
 
 
         }
-    )
+
+
+    })
 
 }
 
 @Composable
-fun drawCircle() {
+private fun drawCircle(modifier: Modifier, teamIcon: Painter, onClick: () -> Unit){
     Box(
-        modifier = Modifier
+        modifier = modifier
             .size(100.dp)
             .border(2.dp, Color.Black, CircleShape)
             .clip(CircleShape)
             .background(MaterialTheme.colorScheme.onPrimaryContainer)
+            .clickable {
+                onClick()
+            }
 
-    )
+    ) {
+        Image(
+            painter = teamIcon,
+            contentDescription = "teamIcon",
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center)
+                .padding(16.dp)
+        )
+
+    }
 
 }
 
@@ -130,7 +149,6 @@ fun PredictionCardPreview() {
     PredictionCard(
         teamOneIcon = painterResource(id = com.example.hltv.R.drawable.astralis_logo),
         teamTwoIcon = painterResource(id = com.example.hltv.R.drawable.astralis_logo),
-        teamOneColor = Color.Red,
-        teamTwoColor = Color.Blue
+
     )
 }
