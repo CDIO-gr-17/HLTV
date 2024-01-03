be5db68206f8dad1a5e4432f39745b080e06f8f9
Jakob-SA <agergaardskovjakob@gmail.com>
2024-01-03 21:37:55 +0100

    You can now only vote once per loaded SingleMatch-page

--- app/src/main/java/com/example/hltv/ui/screens/singleMatch/PredictionCard.kt
+++ app/src/main/java/com/example/hltv/ui/screens/singleMatch/PredictionCard.kt
@@ -21,8 +21,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -33,16 +35,15 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hltv.ui.common.CommonCard
 
-var hasVoted : MutableState<Int>? = null
-var isHomeTeamSelected : MutableState<Boolean> = mutableStateOf(false)
-val borderColor : MutableState<Color> = mutableStateOf(Color.Black)
-val borderWidth : MutableState<Dp> = mutableStateOf(2.dp)
 
+class PredictionCardState {
+    var hasVoted by mutableStateOf(false)
+    var isHomeTeamSelected by mutableStateOf(false)
+}
 @Composable
 fun PredictionCard(
     modifier: Modifier = Modifier,
@@ -52,6 +53,8 @@ fun PredictionCard(
     teamTwoColor: Color = Color.Red,
     viewModel : SingleMatchViewModel
 ) {
+    var hasVoted by remember { mutableStateOf(false)}
+    var isHomeTeamSelected by remember {mutableStateOf(false)}
     CommonCard(modifier = modifier, customInnerPadding = 0.dp, topBox = {
         Box(
             modifier = Modifier
@@ -90,8 +93,7 @@ fun PredictionCard(
                         listOf(
                             teamOneColor, teamTwoColor
                         ),
-
-                        )
+                    )
                 )
         ) {
             Row(
@@ -108,9 +110,14 @@ fun PredictionCard(
                     teamIcon = teamOneIcon,
                     voteCount = viewModel.prediction.value.homeTeamVotePercentage,
                     hometeam = true,
+                    hasVoted = hasVoted,
+                    isHomeTeamSelected = isHomeTeamSelected,
                     onClick = {
-                        hasVoted = mutableStateOf(1)
-                        isHomeTeamSelected.value = true
+                        if (hasVoted) {
+                            return@drawCircle
+                        }
+                        hasVoted = true
+                        isHomeTeamSelected = true
                         viewModel.updatePrediction(1)
                     }
                 )
@@ -129,9 +136,14 @@ fun PredictionCard(
                     teamIcon = teamTwoIcon,
                     voteCount = viewModel.prediction.value.awayTeamVotePercentage,
                     hometeam = false,
+                    hasVoted = hasVoted,
+                    isHomeTeamSelected = isHomeTeamSelected,
                     onClick = {
-                        hasVoted = mutableStateOf(2)
-                        isHomeTeamSelected.value = false
+                        if (hasVoted) {
+                            return@drawCircle
+                        }
+                        hasVoted = true
+                        isHomeTeamSelected = false
                         viewModel.updatePrediction(2)
                     }
                 )
@@ -141,28 +153,26 @@ fun PredictionCard(
 }
 
 @Composable
-private fun drawCircle(modifier: Modifier, teamIcon: Painter, voteCount : Int, hometeam : Boolean,onClick: () -> Unit) {
+private fun drawCircle(
+    modifier: Modifier,
+    teamIcon: Painter,
+    voteCount: Int,
+    hometeam: Boolean,
+    hasVoted: Boolean,
+    isHomeTeamSelected: Boolean,
+    onClick: () -> Unit
+) {
     Column {
-        Box(modifier =
-        if (hasVoted !=null) {
-            modifier
-                .size(100.dp)
-                .border(4.dp, Color.White, CircleShape)
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.onPrimaryContainer)
-                .clickable {
-                    onClick()
-                }
-        } else {
-            modifier
-                .size(100.dp)
-                .border(2.dp, Color.Black, CircleShape)
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.onPrimaryContainer)
-                .clickable {
-                    onClick()
-                }
-        }
+        Box(
+            modifier =
+                modifier
+                    .size(100.dp)
+                    .border(when {hasVoted -> 4.dp else -> 2.dp}, if (hasVoted) Color.White else Color.Black, CircleShape)//here TODO
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.onPrimaryContainer)
+                    .clickable {
+                        onClick()
+                    }
         ) {
             Image(
                 painter = teamIcon,
@@ -173,8 +183,7 @@ private fun drawCircle(modifier: Modifier, teamIcon: Painter, voteCount : Int, h
                     .padding(16.dp)
             )
         }
-        if (hasVoted != null){
-
+        if (hasVoted){
                 Icon(
                     imageVector = Icons.Default.CheckCircle,
                     contentDescription = "mark",
@@ -183,26 +192,24 @@ private fun drawCircle(modifier: Modifier, teamIcon: Painter, voteCount : Int, h
                         .align(Alignment.CenterHorizontally)
                         .offset(y = (-30).dp, x = (25).dp)
                         .size(35.dp)
-                        .alpha(if ((isHomeTeamSelected.value && hometeam) || (!hometeam && !isHomeTeamSelected.value)) {
-                            1f
-                        } else {
-                            0f
-                        })
+                        .alpha(
+                            if ((isHomeTeamSelected && hometeam) || (!hometeam && !isHomeTeamSelected)) {
+                                1f
+                            } else {
+                                0f
+                            }
+                        )
                 )
-
                 Text(
                     text = voteCount.toString() + "%",
                     modifier = Modifier
                         .align(Alignment.CenterHorizontally)
-                        //.padding(8.dp)
                         .offset(y = (-25).dp),
                     fontSize = 20.sp,
                     textAlign = TextAlign.Center,
                 )
             }
     }}
-
-
 @Preview
 @Composable
 fun PredictionCardPreview() {
