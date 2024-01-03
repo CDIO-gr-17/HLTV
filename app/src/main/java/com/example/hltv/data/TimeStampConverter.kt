ede86d3eade5fb367f931c75db6d707013b17b56
Philip <philipmuff@gmail.com>
2024-01-03 09:59:31 +0100

    Changes convertTimestampToDateClock to follow CET

--- app/src/main/java/com/example/hltv/data/TimeStampConverter.kt
+++ app/src/main/java/com/example/hltv/data/TimeStampConverter.kt
@@ -5,6 +5,7 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 class TimeStampConverter {
 }
@@ -31,6 +32,7 @@ fun convertTimestampToDateURL(timestamp: Int?): String {
 
 fun convertTimestampToDateClock(timestamp: Int?): String {
     val dateFormat = SimpleDateFormat("EEEE, dd/MM HH:mm", Locale.getDefault())
+    dateFormat.timeZone = TimeZone.getTimeZone("CET")
     if (timestamp != null) {
         val date = Date(timestamp.toLong() * 1000) // Assuming the timestamp is in seconds, multiply by 1000 for milliseconds
         return dateFormat.format(date)
