41f0ad4514614574cc79602314e5a5c0f6109fbb
Philip <philipmuff@gmail.com>
2024-01-04 09:52:06 +0100

    Made all TimeStampConverters take the timezone into consideration

--- app/src/main/java/com/example/hltv/data/TimeStampConverter.kt
+++ app/src/main/java/com/example/hltv/data/TimeStampConverter.kt
@@ -11,6 +11,7 @@ class TimeStampConverter {
 }
 fun convertTimestampToDateDisplay(timestamp: Int?): String {
     val dateFormat = DateFormat.getDateInstance()
+    dateFormat.timeZone = TimeZone.getTimeZone("CET")
     if(timestamp != null) {
         val date = Date(timestamp.toLong()*1000L)// Assuming the timestamp is in seconds, multiply by 1000 for milliseconds
         return dateFormat.format(date)
@@ -21,6 +22,7 @@ fun convertTimestampToDateDisplay(timestamp: Int?): String {
 
 fun convertTimestampToDateURL(timestamp: Int?): String {
     val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    dateFormat.timeZone = TimeZone.getTimeZone("CET")
     if (timestamp != null) {
         val date =
             Date(timestamp.toLong() * 1000) // Assuming the timestamp is in seconds, multiply by 1000 for milliseconds
