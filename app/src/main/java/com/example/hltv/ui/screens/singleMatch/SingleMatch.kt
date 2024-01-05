5aa7a24fdae53a37cf21bc15997011788e0838b7
Jarl Roest <s224556@dtu.dk>
2024-01-05 09:34:31 +0100

    Single match challengers card added

--- app/src/main/java/com/example/hltv/ui/screens/singleMatch/SingleMatch.kt
+++ app/src/main/java/com/example/hltv/ui/screens/singleMatch/SingleMatch.kt
@@ -2,6 +2,7 @@ package com.example.hltv.ui.screens.singleMatch
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hltv.R
+import com.example.hltv.ui.common.CommonCard
 
 @Composable
 fun SingleMatchScreen(matchID : String?){
@@ -40,6 +43,8 @@ fun SingleMatchScreen(matchID : String?){
         PredictionCard(teamOneIcon = painterResource(id = R.drawable.astralis_logo), teamTwoIcon = painterResource(
             id = R.drawable.astralis_logo
         ) , viewModel = viewModel)
+
+        TeamsInEvent(teamOne = painterResource(id = R.drawable.astralis_logo), teamTwo = painterResource(id = R.drawable.astralis_logo), teamThree = painterResource(id = R.drawable.astralis_logo), teamFour = painterResource(id = R.drawable.astralis_logo), teamFive = painterResource(id = R.drawable.astralis_logo), teamSix = painterResource(id = R.drawable.astralis_logo))
     }
 
 }
@@ -63,16 +68,16 @@ fun EventImage(
         )
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.Center)
+                    .fillMaxWidth()
+                    .align(Alignment.Center)
         ) {
             Spacer(modifier = Modifier.weight(1f))
             Image(
                 painter = teamOneLogo,
                 contentDescription = null,
                 modifier = Modifier
-                    .size(110.dp)
-                    .align(Alignment.CenterVertically)
+                        .size(110.dp)
+                        .align(Alignment.CenterVertically)
             )
             Spacer(modifier = Modifier.weight(1f))
             Column {
@@ -82,8 +87,8 @@ fun EventImage(
                     painter = trophyImage,
                     contentDescription = null,
                     modifier = Modifier
-                        .size(50.dp)
-                        .align(Alignment.CenterHorizontally)
+                            .size(50.dp)
+                            .align(Alignment.CenterHorizontally)
                 )
                 Spacer(modifier = Modifier.weight(1f))
                 Row {
@@ -118,10 +123,10 @@ fun EventImage(
                     fontWeight = FontWeight.Medium,
                     textAlign = TextAlign.Center,
                     modifier = Modifier
-                        .clip(RoundedCornerShape(10.dp))
-                        .background(Color.Red)
-                        .padding(horizontal = 12.dp, vertical = 3.dp)
-                        .align(Alignment.CenterHorizontally)
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(Color.Red)
+                            .padding(horizontal = 12.dp, vertical = 3.dp)
+                            .align(Alignment.CenterHorizontally)
 
                 )
                 Spacer(modifier = Modifier.weight(1f))
@@ -131,16 +136,113 @@ fun EventImage(
                 painter = teamTwoLogo,
                 contentDescription = null,
                 modifier = Modifier
-                    .size(110.dp)
-                    .align(Alignment.CenterVertically)
+                        .size(110.dp)
+                        .align(Alignment.CenterVertically)
             )
             Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
 
+@Composable
+fun TeamsInEvent(
+teamOne: Painter,
+teamTwo: Painter,
+teamThree: Painter,
+teamFour: Painter,
+teamFive: Painter,
+teamSix: Painter
+){
+    CommonCard (
+        modifier = Modifier, topBox = {
+        Box(
+                modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.primaryContainer)
+                        .fillMaxWidth()
+                        .padding(8.dp)
+        ){
+            Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                        text = "Challengers",
+                        fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                )
+            }
+        }
+    }, bottomBox = {
+        Box{
 
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween)
+            {
+                Spacer(modifier = Modifier.weight(0.5f))
+                Column{
+                    Image(
+                            painter = teamOne,
+                            contentDescription = null,
+                            modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                    )
 
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    Image(
+                            painter = teamTwo,
+                            contentDescription = null,
+                            modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                    )
+                }
+                Spacer(modifier = Modifier.weight(0.9f))
+                Column{
+                    Image(
+                            painter = teamThree,
+                            contentDescription = null,
+                            modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                    )
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    Image(
+                            painter = teamFour,
+                            contentDescription = null,
+                            modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                    )
+                }
+                Spacer(modifier = Modifier.weight(0.9f))
+                Column{
+                    Image(
+                            painter = teamFive,
+                            contentDescription = null,
+                            modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                    )
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    Image(
+                            painter = teamSix,
+                            contentDescription = null,
+                            modifier = Modifier
+                                    .size(70.dp)
+                                    .align(Alignment.CenterHorizontally)
+                    )
+                }
+                Spacer(modifier = Modifier.weight(0.5f))
+            }
+        }
+    }
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
