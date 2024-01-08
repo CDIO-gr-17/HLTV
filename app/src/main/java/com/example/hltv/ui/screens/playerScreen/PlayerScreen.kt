5df4ff845519b295e6b8cb4aa3687b0610594587
EsbenSE <s224555@student.dtu.dk>
2024-01-08 16:09:49 +0100

    PlayerScreen added first version

--- app/src/main/java/com/example/hltv/ui/screens/playerScreen/PlayerScreen.kt
+++ app/src/main/java/com/example/hltv/ui/screens/playerScreen/PlayerScreen.kt
@@ -1,37 +1,43 @@
 package com.example.hltv.ui.screens.playerScreen
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.hltv.R
 import com.example.hltv.ui.common.CommonCard
+import com.example.hltv.ui.screens.singleTeamScreen.SingleTeamViewModel
 
 
+@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun PlayerScreen(
     playerIDfullString: String? = "Name"
 ){
-    val playerID = playerIDfullString!!.removePrefix("{playerID}")
-    Log.i("", "Transferred player \"ID\": " + playerID)
+
+    val viewModel : PlayerScreenViewModel = viewModel()
+    LaunchedEffect(Unit){
+        viewModel.loadData(playerIDfullString)
+    }
+
+    Log.i("playerID", "Transferred player \"ID\": " + playerIDfullString)
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -46,26 +52,29 @@ fun PlayerScreen(
                     .fillMaxWidth()
                     .weight(1f)
             ){
-                TeamBox(
-                    team = ("Player: " + playerID),
-                    teamWorldRank = "5",
-                    teamNationality = "Denmark",
-                )
+
+                    TeamBox(
+                        team = viewModel.player.value?.team?.name,
+                        //("Player: " + playerID),
+                        teamWorldRank = "5",
+                        teamNationality = "Denmark",
+                    )
+                }
             }
             Box (modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f))
             {
                 StatsBox(
-                    name = "Dean big mama",
-                    rating = "2,0",
-                    KD = "1,45"
+                    name = viewModel.player.value?.name,
+                    rating = "1.23 test",
+                    KD = "1.23 test"
                 )
             }
 
         }
     }
-}
+
 
 @Composable
 fun PlayerImage(
@@ -82,7 +91,7 @@ fun PlayerImage(
 
 @Composable
 fun TeamBox(
-    team: String,
+    team: String?,
     teamWorldRank: String,
     teamNationality: String,
 ){
@@ -122,6 +131,7 @@ fun StatsBox(
         }
     )
 }
+
 
 @Preview
 @Composable
