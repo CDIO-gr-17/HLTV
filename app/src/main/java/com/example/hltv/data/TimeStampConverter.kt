63ac0b43e9dac9961b3addda7aeb67ee9f88bde5
Philip <philipmuff@gmail.com>
2024-01-11 20:09:23 +0100

    Refactor TimeStampConverter and added CountdownViewModel

--- app/src/main/java/com/example/hltv/data/TimeStampConverter.kt
+++ app/src/main/java/com/example/hltv/data/TimeStampConverter.kt
@@ -1,7 +1,12 @@
 package com.example.hltv.data
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.time.LocalDate
@@ -33,7 +38,7 @@ fun convertTimestampToDateURL(timestamp: Int?): String {
     }
 }
 
-fun convertTimestampToDateClock(timestamp: Int?): String {
+fun convertTimestampToWeekDateClock(timestamp: Int?): String {
     val dateFormat = SimpleDateFormat("EEEE, dd/MM HH:mm", Locale.getDefault())
     dateFormat.timeZone = TimeZone.getTimeZone("CET")
     if (timestamp != null) {
@@ -63,21 +68,48 @@ fun convertYearToUnixTimestamp(stringWithYear: String): Int {
 
 }
 
-fun countdownTimer(timeStamp: Int?): Int {
-    val currentTimeMillis = System.currentTimeMillis()
-    if(timeStamp!=null) {
-        var timeLeft = (timeStamp.toLong() - currentTimeMillis) / 1000
+class CountdownViewModel : ViewModel() {
+    private val _remainingTime = MutableStateFlow<Long?>(null)
+    val remainingTime = _remainingTime.asStateFlow()
 
-        runBlocking {
-            while (timeLeft > 0) {
-                delay(1000L)
-                timeLeft--
+    private var countdownJob: Job? = null
+    fun startCountdown(targetTimestamp: Int) {
+        countdownJob?.cancel()
+
+        countdownJob = viewModelScope.launch {
+            while (System.currentTimeMillis()/1000 < targetTimestamp) {
+                val remainingMillis = targetTimestamp - System.currentTimeMillis()/1000
+                _remainingTime.value = remainingMillis
+                delay(1000)
             }
+            _remainingTime.value = 0 // Countdown reached, set remaining time to 0
         }
-
-        return timeLeft.toInt()
     }
-    else{
-        return 0
+    fun stopCountdown() {
+        _remainingTime.value = null
+        countdownJob?.cancel()
+    }
+    override fun onCleared() {
+        super.onCleared()
+        countdownJob?.cancel()
     }
 }
+
+
+fun convertTimestampToDateClock(timestamp: Int?): String {
+    val dateFormat = SimpleDateFormat("dd/MM HH:mm", Locale.getDefault())
+    dateFormat.timeZone = TimeZone.getTimeZone("CET")
+    if (timestamp != null) {
+        val date = Date(timestamp.toLong() * 1000) // Assuming the timestamp is in seconds, multiply by 1000 for milliseconds
+        return dateFormat.format(date)
+    } else {
+        return "Unknown date"
+    }
+}
+fun Long.formatTime(): String {
+    val hours = this / 3600
+    val minutes = (this % 3600) / 60
+    val remainingSeconds = this % 60
+    return String.format("%02d:%02d:%02d", hours, minutes, remainingSeconds)
+}
+
