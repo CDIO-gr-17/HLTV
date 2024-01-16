75e58efe7b9b9262285962d4c66d33e66fe77656
Jakob-SA <agergaardskovjakob@gmail.com>
2024-01-16 20:55:03 +0100

    Settingsscreen fixed so it doesnt indefinetly collects from store

--- app/src/main/java/com/example/hltv/ui/screens/settingsScreen/SettingsScreenViewModel.kt
+++ app/src/main/java/com/example/hltv/ui/screens/settingsScreen/SettingsScreenViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.hltv.ui.screens.settingsScreen
 
 import android.graphics.Bitmap
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.hltv.data.local.PrefDataKeyValueStore
@@ -10,6 +9,7 @@ import com.example.hltv.data.remote.getTeamNameFromID
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class SettingsScreenViewModel : ViewModel() {
@@ -31,19 +31,13 @@ class SettingsScreenViewModel : ViewModel() {
             }
         }
         viewModelScope.launch(Dispatchers.IO) {
-            dataStore.getFavouriteTeam().collect { int ->
+            val int = dataStore.getFavouriteTeam().first()
+            if (_favoriteTeam.value != int) {
                 _favoriteTeam.value = int
-                Log.d("SettingsScreenViewModel", "Favourite team is now: ${_favoriteTeam.value}")
-                if (_favoriteTeam.value == 0) {
-                    return@collect
+                if (_favoriteTeam.value != 0) {
+                    _favoriteTeamName.value = getTeamNameFromID(_favoriteTeam.value).toString()
+                    _teamLogo.value = getTeamImage(_favoriteTeam.value)
                 }
-                _favoriteTeamName.value = getTeamNameFromID(_favoriteTeam.value).toString()
-                Log.d(
-                    "SettingsScreenViewModel",
-                    "Favourite team name is now: ${_favoriteTeamName.value}"
-                )
-
-                _teamLogo.value = getTeamImage(_favoriteTeam.value)
             }
         }
     }
