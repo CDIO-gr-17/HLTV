b7070f7a02fd4abbd9233b5b66a0b28504559ef0
Philip <philipmuff@gmail.com>
2024-01-16 13:29:50 +0100

    Cleanup of eventscreen and viewmodel

--- app/src/main/java/com/example/hltv/ui/screens/eventsScreen/EventsScreen.kt
+++ app/src/main/java/com/example/hltv/ui/screens/eventsScreen/EventsScreen.kt
@@ -14,21 +14,16 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
-import com.example.hltv.R
 import com.example.hltv.data.convertTimestampToDateDisplay
 import com.example.hltv.data.remote.ThirdUniqueTournament
 import com.example.hltv.ui.common.CommonCard
@@ -56,7 +51,7 @@ fun EventsScreen(onclickSingleEvent: (String?) -> Unit) {
     sortedTournaments.clear()
     sortedTournaments.addAll(viewModel.tournaments.sortedByDescending { it.startDateTimestamp })
     sortedTournaments.forEachIndexed{ index, tournament ->
-        map.put(key = index, value = tournaments.indexOf(tournament))
+        map[index] = tournaments.indexOf(tournament)
     }
 
     LaunchedEffect(viewModel.loading) {
@@ -64,23 +59,23 @@ fun EventsScreen(onclickSingleEvent: (String?) -> Unit) {
     }
 
     Column{
-        if (tournamentSeasons.size != 0) { //Crashes if this isnt there, however not the best way to implement
+        if (tournamentSeasons.size != 0) {
             LazyColumn {
                 itemsIndexed(sortedTournaments) { index, item ->
                     SingleEventCard(
-                        eventTitle = "${item.name.toString()} ${tournamentSeasons[map.get(index)!!][0].name}", //I think 0 is always the most recent season? Not sure tho
+                        eventTitle = "${item.name.toString()} ${tournamentSeasons[map[index]!!][0].name}", //I think 0 is always the most recent season? Not sure tho
                         eventDate = if (item.startDateTimestamp != null && item.endDateTimestamp != null) {
                             "${convertTimestampToDateDisplay(item.startDateTimestamp)} - ${convertTimestampToDateDisplay(item.endDateTimestamp)}"
                         } else {
                             null
                         },
-                        eventLogo = rememberAsyncImagePainter(viewModel.tournamentIcons[map.get(index)!!]),
-                        tier = uniqueTournaments[map.get(index)!!].uniqueTournamentInfo.tier?.uppercase(),
-                        prizePool = uniqueTournaments[map.get(index)!!].uniqueTournamentInfo.totalPrizeMoney,
-                        competitors = uniqueTournaments[map.get(index)!!].uniqueTournamentInfo.numberOfCompetitors,
-                        prizePoolCurrency = uniqueTournaments[map.get(index)!!].uniqueTournamentInfo.totalPrizeMoneyCurrency,
+                        eventLogo = rememberAsyncImagePainter(viewModel.tournamentIcons[map[index]!!]),
+                        tier = uniqueTournaments[map[index]!!].uniqueTournamentInfo.tier?.uppercase(),
+                        prizePool = uniqueTournaments[map[index]!!].uniqueTournamentInfo.totalPrizeMoney,
+                        competitors = uniqueTournaments[map[index]!!].uniqueTournamentInfo.numberOfCompetitors,
+                        prizePoolCurrency = uniqueTournaments[map[index]!!].uniqueTournamentInfo.totalPrizeMoneyCurrency,
                         modifier = Modifier
-                            .clickable { onclickSingleEvent(item.id.toString() + "/" + tournamentSeasons[map.get(index)!!][0].id) }
+                            .clickable { onclickSingleEvent(item.id.toString() + "/" + tournamentSeasons[map[index]!!][0].id) }
                     )
                 }
                 if (loadingState){
@@ -97,7 +92,7 @@ fun EventsScreen(onclickSingleEvent: (String?) -> Unit) {
                     }
                 }
             }
-        } else if (loadingState){ //Only having this (and not the else) fails to render the loading icon as the lazycolumn gets longer. Rep of code kinda hurts but it works
+        } else if (loadingState){ //Only having this (and not the else) fails to render the loading icon as the LazyColumn gets longer. Rep of code kinda hurts but it works
             Column {
                 Spacer(modifier = Modifier.height(10.dp))
                 Row(
@@ -175,7 +170,7 @@ fun SingleEventCard(
                     Text(
                         text = NumberFormat.getNumberInstance(Locale.getDefault())
                             .format(prizePool) +
-                                " ${prizePoolCurrency}",
+                                " $prizePoolCurrency",
                         textAlign = TextAlign.End,
                         modifier = Modifier
                             .padding(8.dp)
